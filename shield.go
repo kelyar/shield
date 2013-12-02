6855783b3d397e5f0ad3b14a3d711b5470e760f9
Evgeniy Kelyarsky <evgenek@wix.com>
2013-12-02 21:44:31 +0200

    cloud storage support

--- shield.go
+++ shield.go
@@ -3,6 +3,7 @@ package shield
 import (
   "appengine"
   "appengine/urlfetch"
+  _ "appengine/blobstore"
   //"appengine/user"
   //"code.google.com/p/google-api-go-client/compute/v1beta16"
   //"code.google.com/p/google-api-go-client/storage/v1beta2"
@@ -12,27 +13,37 @@ import (
 )
 
 const (
-  GCS_URL = "http://static.wix.com"
+  StorageURL = "http://storage.googleapis.com"
+  Bucket  = "wixstaticdev"
 )
 
 func init() {
   http.HandleFunc("/", handler)
 }
 
+func blobKey(fileName string) string {
+  return "/gs/" + Bucket + fileName
+}
+
+func fileUrl(fileName string) string {
+  return StorageURL + "/" + Bucket + fileName
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
     // "/media/ce7c31_f0e70d3996554b4cfeff3d19aa05739b.jpg_srz_170_150_75_22_0.5_1.20_0.00_jpg_srz"
-    image_url := r.URL.String()
+    imagePath := r.URL.String()
 
     c := appengine.NewContext(r)
     client := urlfetch.Client(c)
-    resp, err := client.Head(GCS_URL + image_url)
+
+    resp, err := client.Head(fileUrl(imagePath))
     if err != nil {
       http.Error(w, err.Error(), http.StatusInternalServerError)
       return
 
     } else if resp.StatusCode == 200 { // image exists
       w.Header().Set("Content-Type", "image/jpeg")
-      w.Header().Set("X-AppEngine-BlobKey", "here will be blobkey")
+      w.Header().Set("X-AppEngine-BlobKey", blobKey(imagePath))
       fmt.Fprintln(w, "")
 
     } else if resp.StatusCode == 404 { // no image, do request to compute engine?
