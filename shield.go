18ba1881484809dc528dd5ebb62f4f27e97ae771
Evgeniy Kelyarsky <evgenek@wix.com>
2014-01-10 18:06:05 +0200

    skip favicon request, give blob if found

--- shield.go
+++ shield.go
@@ -3,17 +3,18 @@ package shield
 import (
   "appengine"
   "appengine/urlfetch"
-  _ "appengine/blobstore"
+  "appengine/blobstore"
   //"appengine/user"
   //"code.google.com/p/google-api-go-client/compute/v1beta16"
   //"code.google.com/p/google-api-go-client/storage/v1beta2"
 
   "fmt"
+  "html"
   "net/http"
 )
 
 const (
-  StorageURL = "http://storage.googleapis.com"
+  StorageURL = "https://storage.googleapis.com"
   Bucket  = "wixstaticdev"
 )
 
@@ -21,7 +22,7 @@ func init() {
   http.HandleFunc("/", handler)
 }
 
-func blobKey(fileName string) string {
+func blobFileName(fileName string) string {
   return "/gs/" + Bucket + fileName
 }
 
@@ -31,19 +32,28 @@ func fileUrl(fileName string) string {
 
 func handler(w http.ResponseWriter, r *http.Request) {
     // "/media/ce7c31_f0e70d3996554b4cfeff3d19aa05739b.jpg_srz_170_150_75_22_0.5_1.20_0.00_jpg_srz"
-    imagePath := r.URL.String()
+    imagePath := html.EscapeString(r.URL.Path)
+
+    if imagePath == "/favicon.ico" {
+      http.NotFound(w,r)
+      return
+    }
 
     c := appengine.NewContext(r)
     client := urlfetch.Client(c)
 
     resp, err := client.Head(fileUrl(imagePath))
     if err != nil {
+      c.Errorf(err.Error())
       http.Error(w, err.Error(), http.StatusInternalServerError)
       return
 
     } else if resp.StatusCode == 200 { // image exists
       w.Header().Set("Content-Type", "image/jpeg")
-      w.Header().Set("X-AppEngine-BlobKey", blobKey(imagePath))
+      blobKey, err := blobstore.BlobKeyForFile(c, blobFileName(imagePath))
+      if err == nil {
+        w.Header().Set("X-AppEngine-BlobKey", string(blobKey))
+      }
       fmt.Fprintln(w, "")
 
     } else if resp.StatusCode == 404 { // no image, do request to compute engine?
