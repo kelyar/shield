51777b7f31db9ff48ce8543917c4c07a92574291
Evgeniy Kelyarsky <evgenek@wix.com>
2014-03-27 16:01:22 +0200

    empty template for exif testing

---
+++ exif_test.go
@@ -0,0 +1,14 @@
+package shield_test
+
+import (
+  _ "appengine/aetest"
+  _ "fmt"
+  _ "net/http"
+  _ "net/http/httptest"
+  _ "shield"
+  "testing"
+)
+
+func TestExif (t *testing.T) {
+    wrc = NewWrc(t)
+}
